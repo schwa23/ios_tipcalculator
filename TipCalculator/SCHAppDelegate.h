113226a719280c46b5ae04b4a362af44269b633d
Joshua Dickens <jdickens@jdickens-mbp.local>
2014-02-06 22:12:45 -0800

    Initial Commit

---
+++ TipCalculator/SCHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCHAppDelegate.h
+//  TipCalculator
+//
+//  Created by Joshua Dickens on 2/6/14.
+//  Copyright (c) 2014 Joshua Dickens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by Joshua Dickens on 2/6/14.
+//  Copyright (c) 2014 Joshua Dickens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCHAppDelegate class]));
+    }
+}
